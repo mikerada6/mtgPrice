d1628f31e9f5198a34cdf6b1eb0440e3cc2050dd
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2022-08-26 12:53:22 -0400

    update gitIgnore

---
+++ src/main/java/com/example/mtgprice/MtgPriceApplication.java
@@ -0,0 +1,16 @@
+package com.example.mtgprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public
+class MtgPriceApplication {
+
+    public static
+    void main(String[] args) {
+        SpringApplication.run( MtgPriceApplication.class,
+                               args );
+    }
+
+}
---
+++ src/test/java/com/example/mtgprice/MtgPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mtgprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgPriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
