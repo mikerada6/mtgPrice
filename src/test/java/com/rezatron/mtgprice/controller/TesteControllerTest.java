72b38756eb819f8d74676de0eba5f37fa208a44a
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2022-09-16 22:39:44 -0400

    TesteControllerTest

---
+++ src/test/java/com/rezatron/mtgprice/controller/TesteControllerTest.java
@@ -0,0 +1,31 @@
+package com.rezatron.mtgprice.controller;
+
+import com.rezatron.mtgprice.queue.QueueSender;
+import com.rezatron.mtgprice.service.ScryfallService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith( MockitoExtension.class )
+class TesteControllerTest {
+
+    @Mock
+    QueueSender queueSender;
+    @InjectMocks
+    TesteController testeController;
+
+    @Test
+    void send() {
+
+        String response = testeController.send();
+
+        assertEquals( "ok. done", response );
+
+        verify( queueSender ).send( "test message" );
+    }
+}
